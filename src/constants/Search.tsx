ea0960272c030fdee51d761d48ae826195e91864
DigitalPotential <Carlos@digitalpotential.se>
2023-05-05 21:14:15 +0200

    feat: Created Search bar

---
+++ src/constants/Search.tsx
@@ -0,0 +1,42 @@
+import { useContext, useState } from "react";
+import ModeContext from "../context/ModeContext";
+import CountriesContext from "../context/CountriesContext";
+import { API_URL } from "../utils/api";
+import { motion } from "framer-motion";
+
+const Search = () => {
+    const { mode } = useContext(ModeContext);
+    const { setCountries } = useContext(CountriesContext);
+    const [input, setInput] = useState("");
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`${API_URL}/name/${input}`);
+            const data = await response.json();
+            setCountries(data);
+        } catch (error) {
+            console.log(error);
+        }
+
+        event.currentTarget.reset();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <motion.input
+                whileFocus={{ scale: 1.1 }}
+                className={`${
+                    mode ? "bg-white" : "bg-dark-darkBlue"
+                }  font-[600] px-[100px] py-[15px] w-[100%] md:w-[500px] rounded-md shadow-lg focus:outline-0`}
+                type="text"
+                name="name"
+                placeholder="Search for a country..."
+                onChange={(e) => setInput(e.target.value)}
+            />
+        </form>
+    );
+};
+
+export default Search;
