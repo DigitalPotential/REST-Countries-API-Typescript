a6ac24c0452315af00cdd60bf3f2e4261cdc08a8
DigitalPotential <Carlos@digitalpotential.se>
2023-05-05 21:33:44 +0200

    feat: created Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,47 @@
+import { useContext } from "react";
+import SearcIconWhite from "../assets/SearchIconWhite";
+import SearchIcon from "../assets/SearchIcon";
+import ModeContext from "../context/ModeContext";
+import CountriesContext from "../context/CountriesContext";
 import Countries from "./Countries";
+import Search from "../constants/Search";
+import Filter from "../constants/Filter";
+import { API_URL } from "../utils/api";
+
 const Main = () => {
-    return (
-        <div className="bottom pt-[40px] grid grid-cols-1 justify-items-center md:grid-cols-2 md:gap-10 xxl:gap-0 xxl:grid-cols-4">
-            <Countries />
+  const { mode } = useContext(ModeContext);
+  const { setCountries } = useContext(CountriesContext);
+
+  const getCountryByRegion = async (regionName: string) => {
+    try {
+      const res = await fetch(`${API_URL}/region/${regionName}`);
+      if (!res.ok) throw new Error("Failed...");
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+        console.error((error as Error).message);
+    }
+  };
+
+  return (
+    <main
+      className={`${mode ? "bg-white" : "bg-dark-veryDarkBlue"} ${
+        mode ? "text-light-veryDarkBlue" : "text-white"
+      } px-[20px] md:px-[80px] py-[40px]`}
+    >
+      <div className="top flex flex-col gap-10 xxl:gap-0 xl:flex-row xl:justify-between xl:items-center">
+        <div className="search relative">
+          {mode ? <SearchIcon /> : <SearcIconWhite />}
+          <Search />
         </div>
-    );
+        <Filter onSelect={getCountryByRegion} />
+      </div>
+
+      <div className="bottom pt-[40px] grid grid-cols-1 justify-items-center md:grid-cols-2 md:gap-10 xxl:gap-0 xxl:grid-cols-4">
+        <Countries />
+      </div>
+    </main>
+  );
 };
 
 export default Main;
