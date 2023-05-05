3cacec2e12d38ef511d902ec417a9435ef08daef
DigitalPotential <Carlos@digitalpotential.se>
2023-05-05 20:56:15 +0200

    feat: added SunIcon

---
+++ src/assets/SunIcon.tsx
@@ -0,0 +1,9 @@
+import IonIcon from "@reacticons/ionicons";
+
+const SunIcon = () => {
+    return (
+      <IonIcon style={{color:"white"}} name="sunny-sharp" />
+    )
+  }
+  
+  export default SunIcon;
