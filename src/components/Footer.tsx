08291e68d8be01cedd5aecc37e15989fe01d2d6a
DigitalPotential <Carlos@digitalpotential.se>
2023-05-05 20:53:08 +0200

    feat: Created Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import ModeContext from "../context/ModeContext";
+
+const Footer = () => {
+  const { mode } = useContext(ModeContext);
+  return (
+    <footer>
+      <h4
+        className={`${mode ? "bg-white" : "bg-dark-darkBlue"} ${
+          mode ? "text-dark-veryDarkBlue" : "text-white"
+        } font-[600] text-center py-[20px]`}
+      >
+        &copy; Carlos Manriquez
+      </h4>
+    </footer>
+  );
+};
+
+export default Footer;
