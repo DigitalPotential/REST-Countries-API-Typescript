f1ab0dcd6d2ee47abee0dbe50c00cb413ab4b194
DigitalPotential <Carlos@digitalpotential.se>
2023-05-05 21:37:30 +0200

    feat: added more styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -36,7 +36,6 @@ const Main = () => {
         </div>
         <Filter onSelect={getCountryByRegion} />
       </div>
-
       <div className="bottom pt-[40px] grid grid-cols-1 justify-items-center md:grid-cols-2 md:gap-10 xxl:gap-0 xxl:grid-cols-4">
         <Countries />
       </div>
